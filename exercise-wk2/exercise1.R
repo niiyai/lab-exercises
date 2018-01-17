ca8ca615c00e21b19282afd1a9e3f23c5239bd75
Itsumi Niiyama <niiyai@uw.edu>
2018-01-16 16:06:19 -0800

    lab work

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -9,8 +9,7 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
-
+# the initial variable is not an integer so it can not be added
 install.packages("stringr")
 
 my.line <- "Hey, hey, this is the library"
@@ -18,12 +17,12 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# need to create a variable first before printing
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# inital variable isnt created
 
 
 ## Part 2 - Vector and function practice
@@ -32,15 +31,20 @@ said.the.famous <- paste(my.line, " - ", initial)
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(length1, length2){
+  N <- length1 - length2
+  return("The difference in lengths is ", N)
+}
 
 # Pass two vectors of different length to your `CompareLength` function
-
+CompareLength(1,2)
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
-
+DescribeDifference <- function(length1, length2){
+  
+}
 # Pass two vectors to your `DescribeDifference` function
 
 
